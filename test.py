c08c57b5ef6f26694e95b8c7bac7fcf43b099f38
Vikas G N <46003079+vikasgn2@users.noreply.github.com>
2021-08-23 19:42:58 +0530

    Update test.py
    Added comments and improved code indentation

--- test.py
+++ test.py
@@ -1,24 +1,25 @@
-
-
-
+# Importing necessary packages
 import cv2
 import numpy as np
-from keras.models import load_model
-model=load_model("model2-009.model")
+from tensorflow.keras.models import load_model
 
+# Loading a model generated after training
+model=load_model("model2-007.model")
+
+# Result to be displayed 
 results={0:'without mask',1:'mask'}
 GR_dict={0:(0,0,255),1:(0,255,0)}
 
-rect_size = 4
+#Window size and command to start real-time Detection using camera
+rect_size = 5
 cap = cv2.VideoCapture(0) 
 
-
+#Object detection algorithm for real-time video pointing to it's directory
 haarcascade = cv2.CascadeClassifier(r'C:\Users\goutami\anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
 
 while True:
     (rval, im) = cap.read()
     im=cv2.flip(im,1,1) 
-
     
     rerect_size = cv2.resize(im, (im.shape[1] // rect_size, im.shape[0] // rect_size))
     faces = haarcascade.detectMultiScale(rerect_size)
@@ -31,7 +32,6 @@ while True:
         reshaped=np.reshape(normalized,(1,150,150,3))
         reshaped = np.vstack([reshaped])
         result=model.predict(reshaped)
-
         
         label=np.argmax(result,axis=1)[0]
       
@@ -47,11 +47,5 @@ while True:
 
 cap.release()
 
+#Closes all windows after hitting ESC key
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
